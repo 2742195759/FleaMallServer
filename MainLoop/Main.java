30ad8bc6f6395f6b8682ae175d19206cbb630916
xiongkun <807377414@qq.com>
2018-05-01 10:34:50 +0800

    Update with Main and Client

--- MainLoop/Main.java
+++ MainLoop/Main.java
@@ -22,11 +22,11 @@ public class Main {
 		try {
 			ServerSocket ss = new ServerSocket(3511);
 			while(true) {
-				System.out.printf("Listening for connect") ; 
+				System.out.printf("Listening for connect\n") ; 
 				Socket cli = ss.accept() ;
 				System.out.print("Accept Successful\n");
-				ObjectInputStream ois = new ObjectInputStream(cli.getInputStream()) ;
-				ObjectOutputStream oos = new ObjectOutputStream(cli.getOutputStream()) ; 
+				ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(cli.getInputStream() , 4096)) ;
+				ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(cli.getOutputStream() , 4096)) ; 
 				Message msg = (Message) ois.readObject() ;
 				msg.print();
 				Connection c = mysql.getConn() ;
